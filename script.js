d8189208e51ca812ebe1f6634095e3b9c9c77654
unknown <gouiaaahmad@gmail.com>
2022-06-13 22:59:19 +0100

    commit html

---
+++ script.js
@@ -0,0 +1,10 @@
+var main = document.getElementById('main'); 
+
+
+function putName() {
+    const h2 = document.createElement('h2');
+    h2.innerHTML =  'ahmed ';
+    main.appendChild(h2);
+    console.log("ahmed");
+}
+
