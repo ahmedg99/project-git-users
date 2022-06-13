0ef0696a1565e8cb41eb054598c4a91e008f78fc
unknown <gouiaaahmad@gmail.com>
2022-06-13 23:13:29 +0100

    commit html

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 var main = document.getElementById('main'); 
+var input = document.querySelector('input');
+
+// const nom = input.value;
+
 
 
 function putName() {
     const h2 = document.createElement('h2');
-    h2.innerHTML =  'ahmed ';
+
+    h2.innerHTML =  input.value ;
     main.appendChild(h2);
-    console.log("ahmed");
+    console.log("i");
+
 }
 
